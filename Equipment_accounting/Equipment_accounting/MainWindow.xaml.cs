6a6ffcbae663221e29b6b8719e8e012fcb1eebd2
Николай Евгеньевич Терещенко <moctt123@gmail.com>
2021-03-17 21:41:03 +0300

    Добавление папок
    Папки и ссылки

---
+++ Equipment_accounting/Equipment_accounting/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using Equipment_accounting.Model;
+namespace Equipment_accounting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<users> UserList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            UserList = Helper.Connction.users.ToList();
+        }
+
+        private void forget_psw_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функционал находится в доработке.");
+            //Helper.GoNext(new recoverPassword(), this);
+        }
+
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            Process.GetCurrentProcess().Kill();
+        }
+
+
+        private void log_in_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(EmailTextbox.Text) || String.IsNullOrWhiteSpace(Password.Password))
+            {
+                MessageBox.Show("Должны быть заполнены все поля ");
+                return;
+            }
+            foreach (users u in UserList)
+            {
+
+                if (u.email == EmailTextbox.Text && u.password == Password.Password)
+                {
+                    Helper.CurrentUser = u;
+                    Helper.GoNextM(new WorkingWindow(), this);
+                    return;
+                }
+               else
+                {
+                    MessageBox.Show("Email и/или пароль введен(ы) неверно! ");
+                }
+            }
+            
+        }
+        private void registation_Click(object sender, RoutedEventArgs e)
+        {
+            Helper.GoNextM(new registationWindow(), this);
+        }
+
+
+    }
+}
---
+++ Equipment_accounting/Equipment_accounting/infoWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Equipment_accounting.Model;
+namespace Equipment_accounting
+{
+    /// <summary>
+    /// Логика взаимодействия для infoWindow.xaml
+    /// </summary>
+    public partial class infoWindow : Window
+    {
+        public List<main> MainList { get; set; }
+        
+      
+        public infoWindow(main m)
+        {
+            InitializeComponent();
+
+            MainList = Helper.Connction.main.ToList();
+            
+           idLbl.Content = m.id_main;
+            typeEquipLbl.Content = m.equipment1.type_equipment.name_type;
+            nameEquipLbl.Content = m.equipment1.title_equip;
+            
+            descriptionEquipTxtBlock.Text = m.equipment1.description;
+           
+            placementLbl.Content = m.placement1.place.name_place;
+            datePlaceLbl.Content = m.placement1.date_moving;
+            DataContext = this;
+        }
+
+        
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+      this.Close();
+            return;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+    }
+}
